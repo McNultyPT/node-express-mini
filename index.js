19d58e0e9b28656395386321ced77fe9440d9514
McNultyPT <mcnultypt@gmail.com>
2019-02-18 12:18:11 -0800

    adding express as a dependency

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// initial push
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('\n*** Server Running on http://localhost:4000 ***\n')
+});
