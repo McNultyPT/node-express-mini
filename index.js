c10941d5bc25445c2cc5e3a958f8ce6146abf1aa
McNultyPT <mcnultypt@gmail.com>
2019-02-18 13:34:24 -0800

    working on .post to add a user

--- index.js
+++ index.js
@@ -17,7 +17,29 @@ server.get('/api/users', (req, res) => {
         })
 });
 
+server.post('/api/users', (req, res) => {
+    const name = req.name;
+    const bio = req.bio;
 
+    db
+        .insert(name, bio)
+        .then(user => {
+            if (user) {
+                res.status(201).json({ success: true, user })
+            } else {
+                res.status(400).json({
+                    success: false,
+                    errorMessage: 'Please provide name and bio for the user.'
+                })
+            }
+        })
+        .catch( () => {
+            res.status(500).json({
+                success: false,
+                error: 'There was an error while saving the user to the database.'
+            })
+        })
+});
 
 server.listen(4000, () => {
     console.log('\n*** Server Running on http://localhost:4000 ***\n')
