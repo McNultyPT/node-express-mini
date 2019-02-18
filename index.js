91c785f58a127d12ab0c8b8027be012bada9409f
McNultyPT <mcnultypt@gmail.com>
2019-02-18 12:41:34 -0800

    adding .get request to create a user

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            if (users) {
+                res.status(200).json({ success: true, users })
+            } else {
+                res.status(500).json({
+                    success: false,
+                    message: 'There was an error while saving the user to the database'
+                })
+            }
+        })
+        .catch(({ code, message }) => {
+            res.status(code).json({
+                success: false,
+                message,
+            });
+        });
+});
+
 server.listen(4000, () => {
     console.log('\n*** Server Running on http://localhost:4000 ***\n')
 });
